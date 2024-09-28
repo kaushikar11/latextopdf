ca392c8ec952389a7a6a2306047126541d0a44ca
kaushikar11 <kaushikar2211@gmail.com>
2024-09-28 17:59:01 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use(express.json());
 
 app.post('/convert', (req, res) => {
